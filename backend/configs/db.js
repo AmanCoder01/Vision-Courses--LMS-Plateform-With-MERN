dedefe40d2927597ea15ea471ddeeb01bdc7e002
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2025-09-04 19:18:55 +0530

    bug

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try {
-        await mongoose.connect("mongodb+srv://amancoder:amancoder@cluster0.haoorus.mongodb.net/lms", {
-
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        await mongoose.connect("mongodb+srv://amancoder:amancoder@cluster0.haoorus.mongodb.net/lms")
         console.log("DB connected")
     } catch (error) {
         console.log("DB error", error)
